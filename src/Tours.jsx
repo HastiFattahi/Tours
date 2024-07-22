154f426c45d284d318dddd323459815d3547a9a9
HastiFattahi <hastifattahii21@gmail.com>
2024-07-22 14:16:33 +0330

    title fix

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -3,8 +3,9 @@ const Tours = ({tours,removeTour}) => {
   return (
     <section>
         <div className="title">
+        <h2>our tours</h2>
             <div className="title-underline">
-                 
+                
             </div>
         </div>
         <div className="tours">
